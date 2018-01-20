fa72776f5aa9fbf45c5087959ae6b5137ce109c9
Raymond Sutrisno <raymond@sutrisno.me>
2018-01-19 23:45:15 -0600

    prepared mainfile for function arg parsing

--- main.py
+++ main.py
@@ -2,14 +2,30 @@
 
 # imports 
 from characters import *
-from namer import *
 
+def name_gen(argv):
+
+
+
+
+    from namer import generate_name
+    generate_name('', 1)
+
+    pass
 
 
 def Main():
     print('DnD tool generating characters')
 
-    
+    from os import sys.argv as argv
+    utilities = {
+        'namer':name_gen
+    }
+
+    if argv[1] not in utilities:
+        raise Exception('Unsupported utility: '+argv[1])
+
+    utilities[argv[1]](argv)
 
     pass
 
