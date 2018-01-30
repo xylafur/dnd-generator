e5e2ffa697838ac5b902f312571a2894c5e2c0bd
James Richardson <james.k.richardson@ibm.com>
2018-01-30 17:02:49 -0600

    Added basic terminal mode stuff

--- interactive/dnd_gen_shell.py
+++ interactive/dnd_gen_shell.py
@@ -1,4 +1,15 @@
+import termios
+import tty
+import os
+import sys
+
 from util.parser_util import parser_util
+
+class DNDShellException(Exception):pass
+
+"""**************************Globals**************************"""
+STDIN_FILENO = 0
+
 FUNCTIONS = [
     "chargen", 
     "namer", 
@@ -7,27 +18,40 @@ FUNCTIONS = [
     "stats"
 ]
 
-PROMPT = ">>> "
+PROMPT = "dnd> "
 
-class Shell:
-    def __init__(self):
-        self.history = []
-        last_command = None
+old_term = None 
+height = 0
+width = 0
 
-        while last_command not in ["quit", "q"]:
-            last_command, args = self.get_command()
+def move_cursor(x, y):
+    print("\033[0;0H".format(y, x), end="")
 
-    def get_command(self):
-        command = input(PROMPT)
-        command = command.lower()
+def clear_screen():
+    global height; global width
+    move_cursor(0, 0)
+    for _ in range(height):
+        for _ in range(width):
+            print(' ', end='')
 
-        return (command[0], command[1:])
-
-    def run_command(self, command, params):
-        pass
-
-
+def set_terminal_mode(mode):
+    global old_term
+    if mode == "canonical":
+        termios.tcsetattr(STDIN_FILENO, termios.TCSANOW, old_term)
+    elif mode == "non-canonical":
+        tty.setraw(STDIN_FILENO)
+    else:
+        raise DNDShellException("Invalid Terminal Mode")
+    clear_screen()
+    move_cursor(0, 0)
+    
 def run_interactive():
+    global old_term; global height; global width
+    height, width = list(map(int, os.popen('stty size', 'r').read().split()))
+    old_term = termios.tcgetattr(STDIN_FILENO)
+    set_terminal_mode("non-canonical")
     print("running in interactive mode.  Type help for help, quit to quit")
-    Shell()
+    cur_char = sys.stdin.read(1)
+    set_terminal_mode("canonical")
+
 
