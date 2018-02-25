6b406193a4addc5391c47ca105abbd9108230123
xylafur <jkesleyrichardson@gmail.com>
2018-02-24 21:43:04 -0600

    Moved the health tracker into a module for combat

--- combat/health_tracker.py
+++ combat/health_tracker.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 
+from util.shell_util import move_cursor 
+
+def health_print(to_print, noncanonical=True, lineno=0):
+    if noncanonical:
+        move_cursor(0, lineno)
+    print(to_print)
+    return lineno + 1
 
 class HealthTable:
-    def __init__(self, n):
+    def __init__(self, n, noncanonical=False):
         assert(n>0)
         self.HT = {}
         self.total_max_hp = 0
@@ -16,7 +23,6 @@ class HealthTable:
             self.HT[name] = [chp, hp]
             self.total_max_hp += hp
         print()
-
 
     def print_health_table(self):
         total_curr_hp = 0
@@ -44,11 +50,9 @@ class HealthTable:
                 (100 * total_curr_hp/self.total_max_hp),
                 total_curr_hp, self.total_max_hp, status))
 
-
 def Main():
     ht = HealthTable(2)
     ht.print_health_table()
-
 
 if __name__=='__main__':
     Main()
